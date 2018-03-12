8177675d1772ac0bb73e95a619058f1008405b76
Charlie Greenman <charie@razroo.com>
2018-03-11 20:34:03 -0400

    Create types for author and post.

--- data/schema.js
+++ data/schema.js
@@ -5,7 +5,23 @@ const typeDefs = `
 type Query {
   testString: String
 }
+
+type Author{
+  id: Int
+  firstName: String
+  lastName: String
+  posts: [Post]
+}
+
+type Post {
+  id: Int
+  title: String
+  text: String
+  views: Int
+  author: Author 
+}
 `;
+
 
 const schema = makeExecutableSchema({ typeDefs });
 
