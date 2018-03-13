c5888a504693ba855d79a8b814a4be79fc418142
Charlie Greenman <charie@razroo.com>
2018-03-12 21:10:55 -0400

    Add express-graphql and access control headers, for all access to cors on dev environment.

--- server.js
+++ server.js
@@ -6,8 +6,24 @@ import schema from './data/schema';
 const GRAPHQL_PORT = 3000;
 
 const graphQLServer = express();
+const expressGraphQL = require('express-graphql');
 
-graphQLServer.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+graphQLServer.use('/graphql',(req,res,next)=>{
+
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Headers', 'content-type, authorization, content-length, x-requested-with, accept, origin');
+    res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+    res.header('Allow', 'POST, GET, OPTIONS')
+    res.header('Access-Control-Allow-Origin', '*');
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
+}, expressGraphQL({
+    schema,
+    graphiql: true
+}));
 graphQLServer.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
 graphQLServer.listen(GRAPHQL_PORT, () =>
